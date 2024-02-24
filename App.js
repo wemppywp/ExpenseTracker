3b98211bfb559fb7d54c20d4d6e27dd0c7a20f07
Wemppy Wp <wemppywp@gmail.com>
2024-02-24 20:48:21 +0700

    initial project

---
+++ App.js
@@ -0,0 +1,23 @@
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from 'react';
+import {Text, View} from 'react-native';
+import LoginScreen from './src/screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="LoginScreen"
+          component={LoginScreen}
+          options={{headerShown: false}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
