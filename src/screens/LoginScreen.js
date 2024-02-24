38e0f8dfde86a0612b56b4b939b2ef5eda1f8b89
Wemppy Wp <wemppywp@gmail.com>
2024-02-24 21:55:09 +0700

    add navigation, screens

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,11 +1,14 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const LoginScreen = () => {
   return (
-    <View>
-      <Text className="text-red-400">LoginScreen</Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <Text className="text-red-400">LoginScreen</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
